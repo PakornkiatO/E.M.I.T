2d0acaf3f7c4547d8db0f76f77bda6156e5caa52
PakornkiatO <pakornkiat18@gmail.com>
2025-11-09 23:04:52 +0700

    can reach application from other network

--- client/src/App.js
+++ client/src/App.js
@@ -2,7 +2,7 @@ import { useState } from 'react';
 import './App.css';
 import io from 'socket.io-client';
 
-const socket = io.connect("http://localhost:3001");
+const socket = io.connect("http://124.122.140.111:3001");
 
 function App() {
   const [message, setMessage] = useState("");
--- server/server.js
+++ server/server.js
@@ -9,19 +9,23 @@ const server = http.createServer(app);
 
 const io = new Server(server, {
     cors:{
-        origin: "http://localhost:3000",
+        origin: "http://124.122.140.111:3000",
         methods: ["GET", "POST"]
     }
 });
 
 io.on("connection", (socket) => {
-    console.log(`User connected: ${socket.id}`);
+    console.log("✅ New socket connected:", socket.id);
 
     socket.on("send_message", (data) => {
         console.log(`Message from ${socket.id}: ${data.message}`);
     });
+
+    socket.on("disconnect", () => {
+        console.log(`❌ Disconnected: ${socket.id}`);
+    });
 });
 
 server.listen(3001, () => {
-    console.log("SERVER IS RUNNING ")
+    console.log("🚀 Server is running on http://124.122.140.111:30001");
 });
